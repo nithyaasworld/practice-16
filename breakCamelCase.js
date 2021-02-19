4e66039b123366d8e5551a342450a0343b1b5a28
Nithya <nithyaasworld@gmail.com>
2021-02-19 16:07:47 +0530

    break Camel Case solution

---
+++ breakCamelCase.js
@@ -0,0 +1,18 @@
+function solution(str){
+    if(str[0] === str[0].toUpperCase()) return 'invalid';
+    let result = [str[0]];
+    let camelCaseFound = false;
+    for(let i=1; i<str.length; i++){
+        if(str[i] === str[i].toUpperCase()){ 
+            if(camelCaseFound) return 'invalid'; //as there are more than one UpperCase Character
+            result.push(' ');
+            camelCaseFound = true;
+        }
+        result.push(str[i]);
+    }
+    return result.join('');
+}
+console.log(solution("camelCasing"));  // camel Casing
+console.log(solution("somethingHERE")); // invalid input
+console.log(solution("SomethingHere")); // invalid input
+console.log(solution("somethingHere")); // something Here
