5417d766cb5cbea2f77365856997fd7cc196c56e
Nithya <nithyaasworld@gmail.com>
2021-02-24 07:14:23 +0530

    Added wordGenerator solution

---
+++ 16permutateVowels.js
@@ -0,0 +1,47 @@
+function wordGenerator(str){
+    let vowels = ['a','e','i','o','u'];
+    let vowelPos = findVowelPositions(); // gives us an object like this {0: 'a', 2:'e'}
+   
+    let numberOfVowels = Object.keys(vowelPos).length;
+    let result = [];
+    if(numberOfVowels === 1){
+        vowels.forEach(vowel => {
+            let word = str.split('');
+            word.splice(Number(Object.keys(vowelPos)[0]),1,vowel);
+            result.push(word.join('').toLowerCase());
+        })
+    }else if(numberOfVowels < 1) {
+        result.push(str.toLowerCase());
+    }else{
+        result.push(str.toLowerCase());
+        for(let i=0; i< numberOfVowels; i++){
+            let subResult = [];
+            result.forEach(result => {
+                vowels.forEach(vowel => {
+                    let word = result.split('');
+                    word.splice(Number(Object.keys(vowelPos)[i]),1,vowel);
+                    subResult.push(word.join('').toLowerCase());
+                    
+                })
+            })
+            result.push(...subResult);
+        }
+    }
+    return [... new Set(result)].sort();
+
+    //Helper functions
+    function findVowelPositions(){
+        let vowelPositions = {};
+        str.split('').forEach((letter,i) => {
+            if(vowels.includes(letter)){
+                vowelPositions[i] = letter;
+            }
+        })
+        return vowelPositions;
+    }
+}
+console.log(wordGenerator("Pszczyna"));   //  ['pszczyna', 'pszczyne', 'pszczyni', 'pszczyno', 'pszczynu']
+console.log(wordGenerator("aa"));   //  ['aa', 'ae', 'ai', 'ao', 'au', 'ea', 'ee', 'ei', 'eo', 'eu', 'ia', 'ie', 'ii', 'io', 'iu', 'oa', 'oe', 'oi', 'oo', 'ou', 'ua', 'ue', 'ui', 'uo','uu']
+console.log(wordGenerator("B"));   //  ['b']
+console.log(wordGenerator("Pseczyna"));  
+console.log(wordGenerator("aei"));  
